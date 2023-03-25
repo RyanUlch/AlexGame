4b88b10edb626c79d5fa8ae2fe2d1c91b4bbb7ee
Ryan Ulch <RyanUlchDev@Gmail.com>
2023-03-25 09:30:32 -0400

    Add Example to App.vue for display purposes.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 	import updateLog from './components/UpdateLog/UpdateLog.vue';
 	import { useLogComposable } from './composables/logComposable';
+	import MenuButtons from './components/MenuButtons.vue';
 
 	const { addLogLine } = useLogComposable();
 	addLogLine('This is a new line');
@@ -8,6 +9,34 @@
 
 <template>
 	<updateLog />
+	<MenuButtons
+		:btnInfoArray="[
+			{
+				imgFileName: 'Menu',
+				tooltip: 'this is a tooltip',
+				modalHandler: () => {},
+			},
+			{
+				imgFileName: 'Menu',
+				tooltip: 'this is a tooltip',
+				modalHandler: () => {},
+			},
+			{
+				imgFileName: 'Menu',
+				tooltip: 'this is a tooltip',
+				modalHandler: () => {},
+			},
+			{
+				imgFileName: 'Menu',
+				tooltip: 'this is a tooltip',
+				modalHandler: () => {},
+			},
+			{
+				imgFileName: 'Menu',
+				tooltip: 'this is a tooltip',
+				modalHandler: () => {},
+			},
+		]" />
 </template>
 
 <style scoped></style>
